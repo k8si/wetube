1df85f540e499a45b991b2558fb755ebaea354a8
k8si <ksilvers@umass.edu>
2014-12-04 15:36:48 -0500

    updates

---
+++ peer/src/client/director.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http/httputil"
+	"time"
+)
+
+func invitePeers() {
+	fmt.Println("in invitePeers()....")
+	for _, addr := range knownAddrs {
+		if addr == myipaddr {
+			continue
+		}
+		c := make(chan net.Conn)
+		go invite(addr, c)
+		cf := &tls.Config{Rand: rand.Reader}
+		ssl := tls.Client(<-c, cf)
+		s := make(chan []byte)
+		thing := httputil.NewClientConn(ssl, nil)
+		conn := &connection{socket: thing, send: s}
+		h.register <- conn
+	}
+}
+
+func invite(addr string, c chan net.Conn) {
+	fmt.Println("inviting peer @ ", addr)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr+":"+TCP_PORT)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("tcpaddr ", tcpAddr, "resolved.")
+	tcpConn := tryInvite(tcpAddr)
+	if tcpConn == nil {
+		for {
+			fmt.Println("retrying...")
+			tcpConn := tryInvite(tcpAddr)
+			if tcpConn != nil {
+				break
+			}
+			waitFor := 10
+			time.Sleep(time.Duration(waitFor) * time.Second)
+			continue
+		}
+	}
+	c <- tcpConn
+}
+
+func tryInvite(tcpAddr *net.TCPAddr) *net.TCPConn {
+	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		return nil
+	}
+	return tcpConn
+}
